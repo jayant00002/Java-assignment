3d64aad1b4c1579a0aec99d5e1ea6efbcb1b9066
jayant00002 <98280009+jayant00002@users.noreply.github.com>
2024-02-17 14:39:49 +0530

    init

---
+++ xindus-trade/src/main/java/com/example/xindustrade/XindusTradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.xindustrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XindusTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XindusTradeApplication.class, args);
+	}
+
+}
---
+++ xindus-trade/src/test/java/com/example/xindustrade/XindusTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xindustrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XindusTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
